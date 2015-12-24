d3387defc36b5e140562215534df5eddbe5fc702
atyu30 <ipostfix@gmail.com>
2015-12-24 23:36:16 +0800

    python 100

---
+++ python/pstudy_31.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+'''
+【程序32】
+题目：Press any key to change color, do you want to try it. Please hurry up!
+1.程序分析：　　　　　　　　　　　　
+2.程序源代码：
+不知道写呢 :(,先空着吧
+'''
---
+++ python/pstudy_32.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+'''
+【程序33】
+题目：学习gotoxy()与clrscr()函数　　　
+1.程序分析：
+2.程序源代码：
+不知道如何写 :( 先空着吧
+'''
